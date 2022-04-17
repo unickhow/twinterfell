f4302b5f2053d97148cd87cac0b312286d3242e2
unickhow <unick.zhow@gmail.com>
2022-04-17 17:42:42 +0800

    💡 feat: touring landmarks

--- src/components/modelMap.ts
+++ src/components/modelMap.ts
@@ -1,133 +1,60 @@
 import mapboxgl from 'mapbox-gl'
-import * as THREE from 'three'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { disableAll } from '../utils/helper'
+import { initModel } from '../utils/initModel'
+import landMarks from '../assets/landMarks'
 
-// const BASE_GEO = [121.5400, 24.9855]
 const BASE_GEO = [121.5644, 25.0341]
+const mapStarter = {
+  zoom: 9,
+  center: BASE_GEO
+}
 
 export default function () {
   mapboxgl.accessToken = 'pk.eyJ1IjoidW5pY2tob3ciLCJhIjoiY2wyMG55aGhuMDIyajNicHBkbWRwMzQydyJ9.e9Y-71ja4OrSRqaDzN1AOQ'
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/unickhow/cl21feoqz00gn14o9djcbszg1',
-    zoom: 16.5,
-    center: BASE_GEO,
     pitch: 50,
     antialias: true, // create the gl context with MSAA antialiasing, so custom layers are antialiased
-    attributionControl: false
+    attributionControl: false,
+    ...mapStarter
   })
-
   disableAll(map)
 
-  // parameters to ensure the model is georeferenced correctly on the map
-  const modelOrigin = BASE_GEO
-  const modelAltitude = 0
-  const modelRotate = [Math.PI / 2, 0, 0]
-
-  const modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
-    modelOrigin,
-    modelAltitude
-  )
-
-  // transformation parameters to position, rotate and scale the 3D model onto the map
-  const modelTransform = {
-    translateX: modelAsMercatorCoordinate.x,
-    translateY: modelAsMercatorCoordinate.y,
-    translateZ: modelAsMercatorCoordinate.z,
-    rotateX: modelRotate[0],
-    rotateY: modelRotate[1],
-    rotateZ: modelRotate[2],
-    /* Since the 3D model is in real world meters, a scale transform needs to be
-     * applied since the CustomLayerInterface expects units in MercatorCoordinates.
-     */
-    scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits() * 5
-  }
-
-  // configuration of the custom layer for a 3D model per the CustomLayerInterface
-  const customLayer = {
-    id: '3d-model',
-    type: 'custom',
-    renderingMode: '3d',
-    onAdd: function (map, gl) {
-      this.camera = new THREE.Camera()
-      this.scene = new THREE.Scene()
-
-      // create two three.js lights to illuminate the model
-      const directionalLight = new THREE.DirectionalLight(0xffffff)
-      directionalLight.position.set(0, 90, 100).normalize()
-      this.scene.add(directionalLight)
-
-      const directionalLight2 = new THREE.DirectionalLight(0xffffff)
-      directionalLight2.position.set(0, 70, 100).normalize()
-      this.scene.add(directionalLight2)
-
-      // use the three.js GLTF loader to add the 3D model to the three.js scene
-      const loader = new GLTFLoader()
-      loader.load(
-        // '/src/assets/unickhow_logo.glb',
-        'https://docs.mapbox.com/mapbox-gl-js/assets/34M_17/34M_17.gltf',
-        (gltf) => {
-          this.scene.add(gltf.scene)
-        }
-      )
-      this.map = map
-
-      // use the Mapbox GL JS map canvas for three.js
-      this.renderer = new THREE.WebGLRenderer({
-        canvas: map.getCanvas(),
-        context: gl,
-        antialias: true,
-      })
-
-      this.renderer.autoClear = false
-    },
-    render: function (gl, matrix) {
-      const rotationX = new THREE.Matrix4().makeRotationAxis(
-        new THREE.Vector3(1, 0, 0),
-        modelTransform.rotateX
-      )
-      const rotationY = new THREE.Matrix4().makeRotationAxis(
-        new THREE.Vector3(0, 1, 0),
-        modelTransform.rotateY
-      )
-      const rotationZ = new THREE.Matrix4().makeRotationAxis(
-        new THREE.Vector3(0, 0, 1),
-        modelTransform.rotateZ
-      )
-
-      const m = new THREE.Matrix4().fromArray(matrix)
-      const l = new THREE.Matrix4()
-        .makeTranslation(
-          modelTransform.translateX,
-          modelTransform.translateY,
-          modelTransform.translateZ
-        )
-        .scale(
-          new THREE.Vector3(
-            modelTransform.scale,
-            -modelTransform.scale,
-            modelTransform.scale
-          )
-        )
-        .multiply(rotationX)
-        .multiply(rotationY)
-        .multiply(rotationZ)
-
-      this.camera.projectionMatrix = m.multiply(l)
-      this.renderer.resetState()
-      this.renderer.render(this.scene, this.camera)
-      this.map.triggerRepaint()
-    },
-  }
-
-  map.on('style.load', () => {
-    map.addLayer(customLayer, 'waterway-label')
-    rotateCamera(0)
+  map.on('style.load', async () => {
+    landMarks.forEach(landMark => {
+      map.addLayer(initModel(landMark), 'waterway-label')
+    })
+    for (const mark of landMarks) {
+      const center = [mark.lng, mark.lat]
+      await doFly(center)
+    }
   })
 
-  function rotateCamera(timestamp) {
-    map.rotateTo((timestamp /360) % 360, { duration: 0 })
-    requestAnimationFrame(rotateCamera)
+  function doFly (center, options = {}, cb = null) {
+    return new Promise((resolve, reject) => {
+      map.flyTo({
+        center,
+        zoom: 16,
+        pitch: 50,
+        bearing: 0,
+        speed: 0.5,
+        curve: 1,
+        ...options
+      })
+      map.once('moveend', async () => {
+        await rotateCamera()
+        resolve('done')
+      })
+    })
+  }
+
+  function rotateCamera() {
+    return new Promise((resolve, reject) => {
+      map.rotateTo(180, { duration: 20000 })
+      map.once('moveend', () => {
+        resolve('Done rotate')
+      })
+    })
   }
 }
