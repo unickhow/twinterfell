3f616df246f7dfcc00abd2c540fea954f54d02bf
unickhow <unick.zhow@gmail.com>
2022-04-18 23:20:41 +0800

    💡 feat: initial loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { onMount } from 'solid-js';
 // import initMap from './components/normalMap';
 import initMap from './components/modelMap';
 import MessageBox from './components/messageBox';
+import CoverLoading from './components/coverLoading';
 
 const App: Component = () => {
   onMount(() => {
@@ -11,6 +12,7 @@ const App: Component = () => {
 
   return (
     <>
+      <CoverLoading />
       <div id="map" class="h-screen w-screen"></div>
       <MessageBox />
     </>
---
+++ src/components/coverLoading.tsx
@@ -0,0 +1,34 @@
+import type { Component } from 'solid-js';
+import { createSignal } from 'solid-js';
+import store from '../utils/store';
+
+const CoverLoading: Component = () => {
+  const [isLoading, setIsLoading] = createSignal(true);
+
+  const brandStyle = {
+    'background-image': 'url(https://media.giphy.com/media/QaFKMVEP8FvSo/giphy.gif)',
+    'background-size': 'cover',
+    'background-position': 'center 58%',
+    'background-repeat': 'no-repeat',
+    'background-clip': 'text',
+    '-webkit-background-clip': 'text',
+    'font-size': '10rem',
+    'font-family': "'Changa One', cursive",
+    'letter-spacing': '1rem',
+  }
+
+  store.on('mapLoaded', () => {
+    setIsLoading(false)
+  })
+
+  return (
+    <div class={`
+      ${ isLoading() ? '' : 'done' }
+      cover-loading fixed w-screen h-screen top-0 left-0 bg-[#141414] z-50 flex items-center justify-center
+    `}>
+      <h1 class="cover-loading__title color-transparent font-bold p-16" style={ brandStyle }>TWIN<wbr />TER<wbr />FELL</h1>
+    </div>
+  )
+}
+
+export default CoverLoading;
